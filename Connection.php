42e1686e13c75b6f817b0c5dd9a0913686031de5
nBfour1314 <91709361+nBfour1314@users.noreply.github.com>
2021-11-11 07:58:44 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,4 +1,4 @@
 <?php
-	$conn = pg_connect("postgres://ovspvcwyuvnndd:fb6dde992449fd3e0a0c3aa5fbfa59064663feb2f69a44ae841232790e462a6f@ec2-3-227-149-67.compute-1.amazonaws.com:5432/d3ebce9jtal3sh")
+	$conn = pg_connect("postgres://xlahdxpxsbiuza:aabbdae13b6d79fde27af02a36141e3228f20b230ab591a8a8b016846c24fd52@ec2-34-224-239-147.compute-1.amazonaws.com:5432/d3mbu19sk3rper")
 	or die ("can not connect database".pg_connect_error());
 ?>
